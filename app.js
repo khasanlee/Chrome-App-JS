09d108ab717129e266a0fbd5bf2b8bea36e2bfc1
Khasan <xasaniskandarov2000@gmail.com>
2025-02-12 14:36:14 +0900

    mini calc

---
+++ app.js
@@ -0,0 +1,17 @@
+function plus(a,b) {
+    console.log(a+b)
+}
+function minus(a,b) {
+    console.log(a-b)
+}
+function mult(a,b) {
+    console.log(a*b)
+}
+function dif(a,b) {
+    console.log(a/b)
+}
+
+plus(10,5)
+minus(10,5)
+mult(10,5)
+dif(10,5)
