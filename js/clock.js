a9e3db7a762b82e04f5f987f2cb49f30489b1dc6
Khasan <xasaniskandarov2000@gmail.com>
2025-02-19 19:45:26 +0900

    making clock with dAte

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,14 @@
 const clock = document.querySelector("#clock")
 
 
-function sayHello(params) {
-    console.log("heelo")
-}
 
-setInterval(sayHello, 10000)
+function getClock(params) {
+    const date = new Date()
+    
+    const seconds = date.getSeconds()
+    const minutes = date.getMinutes()
+    const hours = date.getHours()
+    clock.innerText = `${hours}:${minutes}:${seconds}`
+}
+getClock()
+setInterval(getClock,1000)
