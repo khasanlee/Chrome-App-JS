f350ebb8aa5bf4b2a03bdd9e5ea7ace71332e195
Khasan <xasaniskandarov2000@gmail.com>
2025-02-19 16:10:07 +0900

    tricks with classList

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input")
 const link = document.querySelector("a")
+const greeting = document.querySelector("#greeting")
 
 function onLoginSubmit(event) {
     event.preventDefault()
+    loginForm.classList.add("hidden") 
     const username = loginInput.value
-    console.log(username)
+    greeting.innerText = "hello " + username
+    greeting.classList.remove("hidden")
+    console.log(greeting)
 }
 function handleLinkClick(event) {
     event.preventDefault()
