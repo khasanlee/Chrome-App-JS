3ac5b78d189f04d14c0c2847ca76d6e8cb77a624
Khasan <xasaniskandarov2000@gmail.com>
2025-02-19 19:58:03 +0900

    using padStart for the clock fnc

--- js/clock.js
+++ js/clock.js
@@ -4,10 +4,9 @@ const clock = document.querySelector("#clock")
 
 function getClock(params) {
     const date = new Date()
-    
-    const seconds = date.getSeconds()
-    const minutes = date.getMinutes()
-    const hours = date.getHours()
+    const seconds = date.getSeconds().toString().padStart(2,"0")
+    const minutes = date.getMinutes().toString().padStart(2,"0")
+    const hours = date.getHours().toString().padStart(2,"0")
     clock.innerText = `${hours}:${minutes}:${seconds}`
 }
 getClock()
