7ce9767d95548a9517e1188fd40ebf17b4fa3cc9
Khasan <xasaniskandarov2000@gmail.com>
2025-02-23 15:02:22 +0900

    added deleting todo lust fnc by using event.targer

--- js/todo.js
+++ js/todo.js
@@ -12,9 +12,18 @@ function handleToDoSubmit(event) {
 function paintToDo(newTodo) {
     const li = document.createElement("li")
     const span = document.createElement("span")
-    li.appendChild(span)
     span.innerText = newTodo
+    const button = document.createElement("button")
+    button.innerText = "❌"
+    button.addEventListener("click", deleteToDo)
+    li.appendChild(span)
+    li.appendChild(button)
     toDoList.appendChild(li)
+}
+
+function deleteToDo(event) {
+    const li = event.target.parentElement
+    li.remove()
 }
 
 
