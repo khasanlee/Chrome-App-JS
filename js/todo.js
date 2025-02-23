01a767d78d65521ec7b9d75846ff86bd071582c1
Khasan <xasaniskandarov2000@gmail.com>
2025-02-23 18:00:03 +0900

    removing todos from local storage when delete todos

--- js/todo.js
+++ js/todo.js
@@ -37,6 +37,8 @@ function paintToDo(newTodo) {
 function deleteToDo(event) {
     const li = event.target.parentElement
     li.remove()
+    toDos = toDos.filter(toDo => toDo.id !== parseInt(li.id))
+    saveToDos()
 }
 
 
